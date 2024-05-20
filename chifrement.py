47c00d860fe1a30b12153bb74b8b8a81983707a1
Cricri16 <129993793+Cricri16@users.noreply.github.com>
2024-05-20 15:46:12 +0200

    Update chifrement.py

--- chifrement.py
+++ chifrement.py
@@ -1,119 +1,43 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-b = "BCDEFGHIJKLMNOPQRSTUVWXYZAbcdefghijklmnopqrstuvwxyza"
-c = "CDEFGHIJKLMNOPQRSTUVWXYZABcdefghijklmnopqrstuvwxyzab"
-d = "DEFGHIJKLMNOPQRSTUVWXYZABCdefghijklmnopqrstuvwxyzabc"
-e = "EFGHIJKLMNOPQRSTUVWXYZABCDefghijklmnopqrstuvwxyzabcd"
-f = "FGHIJKLMNOPQRSTUVWXYZABCDEfghijklmnopqrstuvwxyzabcde"
-g = "GHIJKLMNOPQRSTUVWXYZABCDEFghijklmnopqrstuvwxyzabcdef"
-h = "HIJKLMNOPQRSTUVWXYZABCDEFGhijklmnopqrstuvwxyzabcdefg"
-i = "IJKLMNOPQRSTUVWXYZABCDEFGHijklmnopqrstuvwxyzabcdefgh"
-j = "JKLMNOPQRSTUVWXYZABCDEFGHIjklmnopqrstuvwxyzabcdefghi"
-k = "KLMNOPQRSTUVWXYZABCDEFGHIJklmnopqrstuvwxyzabcdefghij"
-l = "LMNOPQRSTUVWXYZABCDEFGHIJKlmnopqrstuvwxyzabcdefghijk"
-m = "MNOPQRSTUVWXYZABCDEFGHIJKLmnopqrstuvwxyzabcdefghijkl"
-n = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-o = "OPQRSTUVWXYZABCDEFGHIJKLMNopqrstuvwxyzabcdefghijklmn"
-p = "PQRSTUVWXYZABCDEFGHIJKLMNOpqrstuvwxyzabcdefghijklmno"
-q = "QRSTUVWXYZABCDEFGHIJKLMNOPqrstuvwxyzabcdefghijklmnop"
-r = "RSTUVWXYZABCDEFGHIJKLMNOPQrstuvwxyzabcdefghijklmnopq"
-s = "STUVWXYZABCDEFGHIJKLMNOPQRstuvwxyzabcdefghijklmnopqr"
-t = "TUVWXYZABCDEFGHIJKLMNOPQRStuvwxyzabcdefghijklmnopqrs"
-u = "UVWXYZABCDEFGHIJKLMNOPQRSTuvwxyzabcdefghijklmnopqrst"
-v = "VWXYZABCDEFGHIJKLMNOPQRSTUvwxyzabcdefghijklmnopqrstu"
-w = "WXYZABCDEFGHIJKLMNOPQRSTUVwxyzabcdefghijklmnopqrstuv"
-x = "XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw"
-y = "YZABCDEFGHIJKLMNOPQRSTUVWXyzabcdefghijklmnopqrstuvwx"
-z = "ZABCDEFGHIJKLMNOPQRSTUVWXYzabcdefghijklmnopqrstuvwxy"
-
-def chifr(lettre,lettrcle):
-    #on localise d'abord lettre dans a pour avoir sa position
-    loclett = int(a.find(lettre))
-    # on prend cette lettre dans la liste de la letre de chifr
-    if "a" == lettrcle :
-        bla = a[loclett]
-    else:
-        if "b" == lettrcle :
-            bla = b[loclett]
-        else:
-            if "c" == lettrcle :
-                bla = c[loclett]
-            else:
-                if "d" == lettrcle :
-                    bla = d[loclett]
-                else:
-                    if "e" == lettrcle :
-                        bla = e[loclett]
-                    else:
-                        if "f" == lettrcle :
-                            bla = f[loclett]
-                        else:
-                            if "g" == lettrcle :
-                                bla = g[loclett]
-                            else:
-                                if "h" == lettrcle :
-                                    bla = h[loclett]
-                                else:
-                                    if "i" == lettrcle :
-                                        bla = i[loclett]
-                                    else:
-                                        if "j" == lettrcle :
-                                            bla = j[loclett]
-                                        else:
-                                            if "k" == lettrcle :
-                                                bla = k[loclett]
-                                            else:
-                                                if "l" == lettrcle :
-                                                    bla = l[loclett]
-                                                else:
-                                                    if "m" == lettrcle :
-                                                        bla = m[loclett]
-                                                    else:
-                                                        if "n" == lettrcle :
-                                                            bla = n[loclett]
-                                                        else:
-                                                            if "o" == lettrcle :
-                                                                bla = o[loclett]
-                                                            else:
-                                                                if "p" == lettrcle :
-                                                                    bla = p[loclett]
-                                                                else:
-                                                                    if "q" == lettrcle :
-                                                                        bla = q[loclett]
-                                                                    else:
-                                                                        if "r" == lettrcle :
-                                                                            bla = r[loclett]
-                                                                        else:
-                                                                            if "s" == lettrcle :
-                                                                                bla = s[loclett]
-                                                                            else:
-                                                                                if "t" == lettrcle :
-                                                                                    bla = t[loclett]
-                                                                                else:
-                                                                                    if "u" == lettrcle :
-                                                                                        bla = u[loclett]
-                                                                                    else:
-                                                                                        if "v" == lettrcle :
-                                                                                            bla = v[loclett]
-                                                                                        else:
-                                                                                            if "w" == lettrcle :
-                                                                                                bla = w[loclett]
-                                                                                            else:
-                                                                                                if "x" == lettrcle :
-                                                                                                    bla = x[loclett]
-                                                                                                else:
-                                                                                                    if "y" ==  lettrcle:
-                                                                                                        bla = y[loclett]
-                                                                                                    else:
-                                                                                                        if "z" ==  lettrcle:
-                                                                                                            bla = z[loclett]
+a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!:/.;?,[]"
+def letrechang(let):
+    let = a.find(let)
+    chang = a[let:] + a[:let]
+    return chang
+def lettr(lettrcle,loclett):
+    bla =""
+    var = letrechang(lettrcle)
+    bla = var[loclett]
     return bla
-def chifrgene(clée,mot):
+def chifr(lettre,lettrcle):
+    loclett = int(a.find(lettre))
+    bla = lettr(lettrcle,loclett)
+    return bla
+def chifrgene(clxe,mot):
     result = ""
     depla = 0
-    while len(clée) <= len(mot):
-        clée = clée + clée
+    while len(clxe) <= len(mot):
+        clxe = clxe + clxe
     for corect in mot :
-        result = result + str(chifr(corect,str(clée[depla])))
-        depla = depla +1
+        if a.find(corect) == -1:
+            result = result + corect
+        else:
+            result = result + str(chifr(corect,str(clxe[depla])))
+            depla = depla +1
     return result
-print(chifrgene("lol","rob"))
+def lettra(lettrcle,loclett):
+    bla =""
+    var = letrechang(lettrcle)
+    bla = a[var.find(loclett)]
+    return bla
+def unchifrgene(clxe,mot):
+    result = ""
+    depla = 0
+    while len(clxe) <= len(mot):
+        clxe = clxe + clxe
+    for corect in mot :
+        if a.find(corect) == -1:
+            result = result + corect
+        else:
+            result = result + str(lettra(str(clxe[depla]),corect))
+            depla = depla +1
+    return result
